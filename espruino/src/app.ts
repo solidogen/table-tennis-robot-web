80126a9fa2c464f5dba3a33c45b311c4db3a9673
solidogen <solidogen@gmail.com>
2022-05-22 12:51:57 +0200

    Trying to use rest api

--- espruino/src/app.ts
+++ espruino/src/app.ts
@@ -1,18 +1,37 @@
-import appConfig = require('./app-config')
+import appConfig from "./app-config"
 
 let wifi = require('Wifi')
+let httpClient = require('http')
 
-const ssid = appConfig.default.ssid
-const wifiPassword = appConfig.default.wifiPassword
+const getDiodeStatusEndpoint =
+  'https://table-tennis-robot-web.vercel.app/api/diode' 
+  // 'https://google.com'
+
+const ssid = appConfig.ssid
+const wifiPassword = appConfig.wifiPassword
+
+function fetchDiodeStatus() {
+  httpClient.get(getDiodeStatusEndpoint, function (res: any) {
+    var responseBody = ""
+
+    res.on('data', function (data: any) {
+      console.log('\nHTTP:\n\n' + data) // current output -> HTTP: Redirecting to https://table-tennis-robot-web.vercel.app/api/diode (308)
+      responseBody += data
+    })
+    res.on('close', function (data: any) {
+      console.log('\nConnection closed:\n\n' + data) // current output -> Connection closed:false
+      console.log("\n\n")
+      console.log("Full response body: " + responseBody)
+    })
+  })
+}
 
 function connectToWifi() {
   wifi.setHostname('esp-tt')
   wifi.connect(ssid, { password: wifiPassword }, function (err?: string) {
     console.log('connected? err=', err, 'info=', wifi.getIP())
+    fetchDiodeStatus()
   })
-
-  // not needed
-  wifi.stopAP()
   wifi.save()
 }
 
