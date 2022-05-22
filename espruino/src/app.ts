b6d3eda134b37c3313223c0cac18f64ba552269a
solidogen <solidogen@gmail.com>
2022-05-22 14:39:45 +0200

    Really trying to use rest api

--- espruino/src/app.ts
+++ espruino/src/app.ts
@@ -4,8 +4,10 @@ let wifi = require('Wifi')
 let httpClient = require('http')
 
 const getDiodeStatusEndpoint =
-  'https://table-tennis-robot-web.vercel.app/api/diode' 
-  // 'https://google.com'
+  'https://table-tennis-robot-web.vercel.app/api/diode' // (everything gets 308, vercel does some stupid hacks I think)
+  // 'https://vercel.app'  (same behavior, always 308, it's whole site behavior)
+  // 'https://google.com'  (works fine - data in chunks, gets html)
+  // 'https://weightreductor.herokuapp.com/'  (works fine - gets plain text from rest)
 
 const ssid = appConfig.ssid
 const wifiPassword = appConfig.wifiPassword
@@ -14,14 +16,31 @@ function fetchDiodeStatus() {
   httpClient.get(getDiodeStatusEndpoint, function (res: any) {
     var responseBody = ""
 
+    // todo - extract api call to method with callback
+    // todo - add Accept */* header
+
     res.on('data', function (data: any) {
-      console.log('\nHTTP:\n\n' + data) // current output -> HTTP: Redirecting to https://table-tennis-robot-web.vercel.app/api/diode (308)
+      console.log('\nHTTP:\n' + data) // current output -> HTTP: Redirecting to https://table-tennis-robot-web.vercel.app/api/diode (308)
       responseBody += data
     })
-    res.on('close', function (data: any) {
-      console.log('\nConnection closed:\n\n' + data) // current output -> Connection closed:false
+    res.on('close', function (hadError: boolean) {
+      console.log('\nConnection closed. Had error: ' + hadError)
       console.log("\n\n")
       console.log("Full response body: " + responseBody)
+
+      console.log("Res:")
+      Object.keys(res).forEach((prop)=> console.log(prop));
+
+      console.log("Res status: " + res.statusCode)
+      console.log("Res message: " + res.statusMessage)
+      console.log("Res headers: " + JSON.stringify(res.headers))
+
+      // if (res.statusCode == 308) {
+      //   fetchDiodeStatus()
+      // }
+    })
+    res.on('error', function (data: any) {
+      console.log('\nERROR:\n' + data)
     })
   })
 }
