728d2e7fd1358ef756cf0582b5c79f8b8e712478
solidogen <solidogen@gmail.com>
2022-05-25 00:43:55 +0200

    Extracted wifimanager but says module not found

--- espruino/src/app.ts
+++ espruino/src/app.ts
@@ -1,14 +1,9 @@
-import appConfig from './app-config'
 import { Log } from './utils/Log'
+import { WifiManager } from "./utils/WifiManager"
 
-let wifi = require('Wifi')
 let httpClient = require('http')
 
-const ssid = appConfig.ssid
-const wifiPassword = appConfig.wifiPassword
-
 const getDiodeStatusEndpoint = 'https://tt-robot-web.herokuapp.com/api/diode'
-
 
 function setDiodeStatus(isOn: boolean) {
   NodeMCU.D0.write(isOn)
@@ -51,22 +46,8 @@ function fetchDiodeStatus() {
   request.on('error', function (data: any) {
     Log.withFirstLb('Request error:\n' + data) // I have an error here after reconnecting the device, visible in printObject
     Log.printObject(data)
-    connectToWifi(() => fetchDiodeStatus()) // this is probably overkill, I need some return type with extra data from networking wrapper
+    WifiManager.connectToWifi(() => fetchDiodeStatus()) // this is probably overkill, I need some return type with extra data from networking wrapper
   })
-}
-
-function connectToWifi(onSuccess: () => any) {
-  wifi.setHostname('esp-tt')
-  wifi.connect(ssid, { password: wifiPassword }, function (error?: string) {
-    if (error) {
-      Log.normal('Failed to connect to wifi', error)
-      setTimeout(() => connectToWifi(onSuccess), 500)
-    } else {
-      Log.normal('Connected to wifi', wifi.getIP())
-      onSuccess()
-    }
-  })
-  wifi.save()
 }
 
 function setupDiode() {
@@ -78,7 +59,7 @@ function onInit() {
   Log.withFirstLb('I: DEVICE LOGIC STARTS')
   setInterval(() => Log.withFirstLb('I: device running'), 2000)
   setupDiode()
-  connectToWifi(() => fetchDiodeStatus())
+  WifiManager.connectToWifi(() => fetchDiodeStatus())
 }
 
 save() // saves the code to flash memory, then restarts and calls onInit(). this way it works similar to Arduino
